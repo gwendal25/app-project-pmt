b62e683ae18d873a6bc0075a9a0849ba2116fe2f
GwendalArs <g.breton@ars-metal.com>
2026-01-30 15:42:53 +0100

    doc📝: added documentation to task service

--- src/app/services/task-service.ts
+++ src/app/services/task-service.ts
@@ -21,6 +21,11 @@ export class TaskService {
     })
   }
 
+  /**
+   * Envoie une requête au serveur pour récupérer les informations d'une tâche via son id
+   * @param {number} id l'id de la tâche
+   * @returns {Observable<TaskInfo>} Un observable des informations de la tâche (id, nom, description, priorité, status, date de fin, entrées d'historique)
+   */
   getTaskInfoById(id: number): Observable<TaskInfo> {
     return this.httpClient.get<TaskInfo>(this.endpoint + "/" + id)
     .pipe(
@@ -28,6 +33,11 @@ export class TaskService {
     )
   }
 
+  /**
+   * Envoie une requête au serveur pour récupérer les informations d'une tâche sans son historique via son id
+   * @param {number} id l'id de la tâche 
+   * @returns {Observable<TaskInfo>} Un observable des informations de la tâche (id, nom, description, priorité, status, date de fin)
+   */
   getTaskInfoWithoutHistoryById(id: number): Observable<SimpleTaskInfo> {
     return this.httpClient.get<SimpleTaskInfo>(this.endpoint + "/" + id + "/no-history")
     .pipe(
@@ -35,19 +45,42 @@ export class TaskService {
     )
   }
 
-  updateTask(id: number, taskDto: TaskDto) {
+  /**
+   * Envoie une requête au serveur pour mettre à jour une tâche
+   * @param {number} id l'id de la tâche
+   * @param {TaskDto} taskDto Un Dto contenant les informations de mise à jour de la tâche
+   * @returns {Observable<TaskInfo>} Un observable des informations de la tâche (id, nom, description, priorité, status, date de fin)
+   */
+  updateTask(id: number, taskDto: TaskDto): Observable<TaskInfo> {
     return this.httpClient.put<TaskInfo>(this.endpoint + "/" + id, JSON.stringify(taskDto), this.httpOptions);
   }
 
-  assignTask(id:number, userId:number) {
+  /**
+   * Envoie une requête au serveur pour assigner une tâche
+   * @param {number} id l'id de la tâche
+   * @param {number} userId l'id de l'utilisateur
+   * @returns {Observable<ProjectTaskInfo>} Un observable des informations de la tâche (id, nom, description, priorité, status, date de fin, utilisateur assigné, notifications)
+   */
+  assignTask(id:number, userId:number): Observable<ProjectTaskInfo> {
     return this.httpClient.put<ProjectTaskInfo>(this.endpoint + "/" + id + "/assign", JSON.stringify({userId: userId}), this.httpOptions);
   }
 
-  unassignTask(id: number) {
+  /**
+   * Envoie une requête au serveur pour dé-assigner une tâche
+   * @param {number} id l'id de la tâche
+   * @returns {Observable<ProjectTaskInfo>} Un observable des informations de la tâche (id, nom, description, priorité, status, date de fin, utilisateur assigné, notifications)
+   */
+  unassignTask(id: number): Observable<ProjectTaskInfo> {
     return this.httpClient.put<ProjectTaskInfo>(this.endpoint + "/" + id + "/unassign", this.httpOptions);
   }
 
-  setNotificationTask(id: number, isNotified: boolean) {
+  /**
+   * Envoie une requête au serveur pour changer le statut des notifications d'une tâche
+   * @param {number} id l'id de la tâche
+   * @param {boolean} isNotified le status des notifications
+   * @returns {Observable<TaskNotificationDto>} Un observable du status des notifications de la tâche
+   */
+  setNotificationTask(id: number, isNotified: boolean): Observable<TaskNotificationDto> {
     return this.httpClient.put<TaskNotificationDto>(this.endpoint + "/" + id + "/set-assign-notifications", JSON.stringify({ isNotified: isNotified }), this.httpOptions);
   }
 
