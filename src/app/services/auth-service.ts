4e27aef3072b1e69a1317c6bdffda17e57cecf21
GwendalArs <g.breton@ars-metal.com>
2026-01-30 15:42:19 +0100

    doc📝: added documentation to auth service

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { UserDto } from '../interfaces/user/userDto';
 import { User } from '../interfaces/user/user';
 import { UserLoginDto } from '../interfaces/user/userLoginDto';
@@ -19,14 +19,27 @@ export class AuthService {
     })
   }
 
-  createUser(userDto: UserDto) {
+  /**
+   * Envoie une requête pour créer un nouvel utilisateur
+   * @param {UserDto} userDto Un objet contenant les informations de l'utilisateur à créer (nom, email, mot de passe)
+   * @returns {Observable<User>} Les informations de l'utilisateur qui vient d'être crée
+   */
+  createUser(userDto: UserDto): Observable<User> {
     return this.httpClient.post<User>(this.endpoint, JSON.stringify(userDto), this.httpOptions);
   }
 
+  /**
+   * Envoie une requête pour se connecter au serveur 
+   * @param {UserLoginDto} userLoginDto Un objet contenant les informations de connection (email, mot de passe)
+   * @returns Les informations de l'utilisateur
+   */
   login(userLoginDto: UserLoginDto) {
     return this.httpClient.post<User>(this.endpoint + "/login", JSON.stringify(userLoginDto), this.httpOptions);
   }
 
+  /**
+   * Efface les données de connection de l'utilisateur du stockage local (faux token, id, email, nom)
+   */
   logout() {
     localStorage.removeItem('fake-token');
     localStorage.removeItem('id');
@@ -34,10 +47,19 @@ export class AuthService {
     localStorage.removeItem('name');
   }
 
+  /**
+   * Permet de vérifier si le stockage local contient le token de connection
+   * @returns `true` si l'utilisateur est connecté, `false` sinon
+   */
   isLoggedIn() {
     return !!localStorage.getItem('fake-token');
   }
 
+  /**
+   * Internal error handler
+   * @param {any} error the error returned by the function that called this handler
+   * @returns An exception with the error message of the error
+   */
   handleError(error:any) {
     let errorMessage = '';
 
