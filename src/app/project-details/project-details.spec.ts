74c0277b8ae5892218138967ad0406ad167508c5
GwendalArs <g.breton@ars-metal.com>
2026-02-06 15:46:16 +0100

    tests 🧪 : add tests to project details

--- src/app/project-details/project-details.spec.ts
+++ src/app/project-details/project-details.spec.ts
@@ -11,8 +11,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from '../services/project-service';
 import { TaskService } from '../services/task-service';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { UserRole } from '../enums/userRole';
+import { TaskPriority } from '../enums/taskPriority';
+import { TaskStatus } from '../enums/taskStatus';
+import { MatChip, MatChipSet } from '@angular/material/chips';
+import { ProjectTaskInfo } from '../interfaces/task/projectTask';
 
 describe('ProjectDetails', () => {
   let component: ProjectDetails;
@@ -36,6 +40,8 @@ describe('ProjectDetails', () => {
         MatFormFieldModule,
         MatSelectModule,
         MatCheckboxModule,
+        MatChipSet,
+        MatChip,
         RouterTestingModule
       ],
       providers: [
@@ -49,19 +55,40 @@ describe('ProjectDetails', () => {
     component = fixture.componentInstance;
     projectServiceSpy = TestBed.inject(ProjectService) as jasmine.SpyObj<ProjectService>;
     taskServiceSpy = TestBed.inject(TaskService) as jasmine.SpyObj<TaskService>;
-
-    // Mock projectInfo data
+    
+     // Mock projectInfo data
     const mockProjectInfo = {
       id: 1,
       name: 'Test Project',
       description: 'A test project',
       startDate: new Date(),
       userRole: UserRole.ADMIN,
-      tasks: [],
+      tasks: [
+        {
+          id: 1,
+          name: 'Task 1 : design',
+          description: 'design the plans',
+          taskPriority: TaskPriority.HIGH,
+          taskStatus: TaskStatus.REQUIRE_EVALUATION,
+          endDate: new Date(),
+          user: { id: -1, name: ""},
+          notified: false
+        },
+        {
+          id: 2,
+          name: 'Task 2 : verify',
+          description: 'verify the plans',
+          taskPriority: TaskPriority.MEDIUM,
+          taskStatus: TaskStatus.NOT_STARTED,
+          endDate: new Date(),
+          user: { id: -1, name: ""},
+          notified: false
+        }
+      ],
       users: []
     };
     projectServiceSpy.getProjectInfoById.and.returnValue(of(mockProjectInfo));
-
+    
     fixture.detectChanges();
   });
 
@@ -84,4 +111,55 @@ describe('ProjectDetails', () => {
     expect(component.isAdminOrMember(UserRole.MEMBER)).toBeTrue();
     expect(component.isAdminOrMember(UserRole.OBSERVER)).toBeFalse();
   });
+
+  it('should filter tasks', () => {
+    component.filterTasks(TaskStatus.REQUIRE_EVALUATION);
+    expect(component.filteredTaskList[0].name).toEqual('Task 1 : design');
+  })
+
+  it('should call assign task', () => {
+    const projectTaskInfo: ProjectTaskInfo = {
+      id: 1,
+      name: 'Task 1 : design',
+      description: 'design the plans',
+      taskPriority: TaskPriority.HIGH,
+      taskStatus: TaskStatus.REQUIRE_EVALUATION,
+      endDate: new Date(),
+      user: { id: 1, name: "Jane Doe"},
+      notified: false
+    }
+    taskServiceSpy.assignTask.and.returnValue(of(projectTaskInfo));
+    component.onUserAssignedSelected(1, 1);
+    expect(taskServiceSpy.assignTask).toHaveBeenCalled();
+  })
+
+  it('should call unassign task', () => {
+    const projectTaskInfo = {
+      id: 1,
+      name: 'Task 1 : design',
+      description: 'design the plans',
+      taskPriority: TaskPriority.HIGH,
+      taskStatus: TaskStatus.REQUIRE_EVALUATION,
+      endDate: new Date(),
+      user: {id: -1, name: "" },
+      notified: false
+    }
+    taskServiceSpy.unassignTask.and.returnValue(of(projectTaskInfo));
+    component.onUserAssignedSelected(10, -1);
+    expect(taskServiceSpy.unassignTask).toHaveBeenCalled();
+  })
+
+  it('should call set notification task with true', () => {
+    const taskNotificationDto = { isNotified: true }
+    taskServiceSpy.setNotificationTask.and.returnValue(of(taskNotificationDto));
+    component.updateNotificationStatus(1, true);
+    expect(taskServiceSpy.setNotificationTask).toHaveBeenCalled();
+  })
+
+  it('should call set notification task with false', () => {
+    const taskNotificationDto = { isNotified: false }
+    taskServiceSpy.setNotificationTask.and.returnValue(of(taskNotificationDto));
+    component.updateNotificationStatus(1, true);
+    expect(taskServiceSpy.setNotificationTask).toHaveBeenCalled();
+  })
 });
